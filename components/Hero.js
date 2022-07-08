fb3af144d8c76d789f99d24df72dcc1acdb593da
Mo-Aziz <mohamed.aref32@gmail.com>
2022-07-08 15:12:41 -0500

    deleted boiler plate code and added hero section with background img

---
+++ components/Hero.js
@@ -0,0 +1,13 @@
+export const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img ">
+      {/* overlay */}
+      <div />
+      <div>
+        <h3>Heading</h3>
+        <p>Message</p>
+        <button>Book</button>
+      </div>
+    </div>
+  );
+};
