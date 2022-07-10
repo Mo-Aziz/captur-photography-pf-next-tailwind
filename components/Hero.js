4deb0c0d91c90982a94657f7ef3e345ef864b221
Mo-Aziz <mohamed.aref32@gmail.com>
2022-07-10 11:03:27 -0500

    added the gallery section and instagram section

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,12 @@
-export const Hero = () => {
+export const Hero = ({heading,message}) => {
   return (
     <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img ">
       {/* overlay */}
-      <div />
-      <div>
-        <h3>Heading</h3>
-        <p>Message</p>
-        <button>Book</button>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
+      <div className='p-5 text-white z-[2]   mt-[-10rem]'>
+        <h3 className='text-5xl font-bold'>{heading}</h3>
+        <p className='py-5 text-xl'>{message}</p>
+        <button className='px-8 py-2 border'>Book</button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Hero } from '../components/Hero'
+import Instagram from '../components/Instagram';
+import Slider from '../components/Slider'
+import { SliderData } from '../components/SliderData';
+ 
  
 
 export default function Home() {
@@ -11,8 +15,11 @@ export default function Home() {
         <meta name="photography mock up" content="next js web app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+    
 
-       <Hero/>
+       <Hero heading="Captur Photography" message="I enjoy every shot and every shot i take can express itself."/>
+       <Slider slides={SliderData}/>
+       <Instagram/>
     </div>
   )
 }
